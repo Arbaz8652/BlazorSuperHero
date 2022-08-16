dc27c3010a0d76e6d1e8aa1f7aba13ccfe8021f7
arbaaz <arbaaz@ARBAAZPATHAN>
2022-08-16 23:47:31 +0530

    Add project files.

---
+++ BlazorSuperHero/Client/Services/SuperHeroServices/ISuperHeroService.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorSuperHero.Client.Services.SuperHeroServices
+{
+    public interface ISuperHeroService
+    {
+        List<SuperHero> Heroes { get; set; }
+        List<Comic> Comics { get; set; }
+        Task GetComic();
+        Task GetSuperHero();
+        Task<SuperHero>GetSingleHero(int id);
+
+        Task CreatHero(SuperHero hero);
+
+        Task UpdateHero(SuperHero hero);
+        Task DeleteHero(int id);
+
+    }
+}
